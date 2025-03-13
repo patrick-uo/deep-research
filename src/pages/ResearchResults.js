3fa1434f1ff34a3a73dfce4dec6650f8c1a017da
patrick-uo <patrick_luo@foxmail.com>
2025-03-13 17:55:02 +0800

    Create ResearchResults.js
    add ResearchResults.js

---
+++ src/pages/ResearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+const ResearchResults = () => {  
+  return (  
+    <div>  
+      <h2>Research Results</h2>  
+      <p>This is where the research results will be displayed.</p>  
+    </div>  
+  );  
+};  
+
+export default ResearchResults;
