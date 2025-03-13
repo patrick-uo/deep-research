55e701db08d6a79f5fc181a0381f32fca89369b4
patrick-uo <patrick_luo@foxmail.com>
2025-03-13 17:54:29 +0800

    Create Home.js
    add Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';  
+import ResearchForm from '../components/ResearchForm';  
+
+const Home = () => {  
+  return (  
+    <div>  
+      <h2>Welcome to Deep Research</h2>  
+      <ResearchForm />  
+    </div>  
+  );  
+};  
+
+export default Home;
