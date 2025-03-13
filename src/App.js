c57d44077aa0a10bd88e517d606fc4716e370a6f
patrick-uo <patrick_luo@foxmail.com>
2025-03-13 18:49:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from 'react';  
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'; // 注意这里使用 HashRouter  
-import Header from './components/Header';  
-import Sidebar from './components/Sidebar';  
-import Home from './pages/Home';  
-import ResearchResults from './pages/ResearchResults';  
-import { Layout } from 'antd';  
-
-const { Content } = Layout;  
+import { BrowserRouter as Router } from 'react-router-dom';  
 
 function App() {  
   return (  
-    <Router basename={process.env.PUBLIC_URL}>  
-      <Layout>  
-        <Header />  
-        <Layout>  
-          <Sidebar />  
-          <Content style={{ padding: '24px' }}>  
-            <Switch>  
-              <Route path="/" exact component={Home} />  
-              <Route path="/results" component={ResearchResults} />  
-            </Switch>  
-          </Content>  
-        </Layout>  
-      </Layout>  
+    <Router>  
+      <div>Deep Research App</div>  
     </Router>  
   );  
 }  
 
-export default App;
+export default App;  
