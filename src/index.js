ef8b8b2c79128ae1a245f14db97db194e3cf404c
patrick-uo <patrick_luo@foxmail.com>
2025-03-13 18:48:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';  
 import ReactDOM from 'react-dom';  
 import App from './App';  
-import 'antd/dist/antd.css'; // Ant Design 样式  
-import './index.css';  
 
 ReactDOM.render(  
   <React.StrictMode>  
     <App />  
   </React.StrictMode>,  
   document.getElementById('root')  
-);
+);  
